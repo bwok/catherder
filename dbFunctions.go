252a306c340b650db671ffc5f094ecdefe24383f
james <james.fuge@gmail.com>
2019-02-11 11:55:47 +1300

    Check and open the database file. Added structs for holding database data when getting/creating/updating.

--- dbFunctions.go
+++ dbFunctions.go
@@ -2,3 +2,32 @@ package main
 
 // extra database functions that don't live in crud,
 // database struct definitions, custom sql etc.
+
+type User struct {
+	Id        int64
+	IdDate    int64
+	Name      string `json:"name"`
+	Available bool   `json:"available"`
+}
+type Users []User
+type Date struct {
+	Id       int64
+	IdMeetUp int64
+	Date     int64 `json:"date"`
+	Users    Users `json:"users"`
+}
+type Dates []Date
+type Admin struct {
+	Id       int64
+	IdMeetUp int64
+	Email    string `json:"email"`
+	Alerts   bool   `json:"alerts"`
+}
+type MeetUp struct {
+	Id          int64
+	UserHash    string
+	AdminHash   string
+	Dates       Dates  `json:"dates"`
+	Admin       Admin  `json:"admin"`
+	Description string `json:"description"`
+}
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
+)
+
+var db *sql.DB
+
 func main() {
+	var err error
+
+	// Check the database file is present, open it
+	if _, err = os.Stat("./data.sqlite"); err == nil {
+		if db, err = sql.Open("sqlite3", "./data.sqlite"); err != nil {
+			log.Fatal(err)
+		}
+	} else if os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+	defer func() {
+		if closeErr := db.Close(); closeErr != nil {
+			log.Println(closeErr)
+		}
+	}()
 
 }
