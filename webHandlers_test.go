08d2cf8241f3ecf2c9682e211abba430a1bea025
james <james.fuge@gmail.com>
2019-02-24 21:24:37 +1300

    add test for the pageNewHandler http handler function.

--- webHandlers_test.go
+++ webHandlers_test.go
@@ -1,9 +1,45 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
 
 func TestPageNewHandler(t *testing.T) {
-	// TODO test
+	request := httptest.NewRequest("GET", "https://localhost/new.html", nil)
+	w := httptest.NewRecorder()
+	pageNewHandler(w, request)
+
+	response := w.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+
+	if response.StatusCode != 200 {
+		t.Error("http status code was not 200")
+	}
+	if response.Header.Get("Content-Type") != "text/html; charset=utf-8"{
+		t.Error("Content-Type header was wrong")
+	}
+	if response.Header.Get("Strict-Transport-Security") != "max-age=31536000; includeSubDomains"{
+		t.Error("Strict-Transport-Security header was wrong")
+	}
+	if response.Header.Get("Content-Security-Policy") != "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';"{
+		t.Error("Content-Security-Policy header was wrong")
+	}
+
+	file, err := os.Open("templates/new.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+	htmlBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if bytes.Compare(body, htmlBytes) != 0 {
+		t.Error("returned bytes for new.html differed to the one found on the filesystem.")
+	}
 }
 
 func TestAjaxCreateHandler(t *testing.T) {
